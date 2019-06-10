6597fde4dd9755b88d3e0a343b08f53aee7b8f07
rubecube <rubcube2010@gmail.com>
2019-06-10 20:40:55 +0000

    Change the myscript file to add more object oriented

--- my_script.py
+++ my_script.py
@@ -1,5 +1,11 @@
-print 3+4
-print 4+4
-print 3+4
-print 4+4
-print 9+4
+from __future__ import print_function
+
+def main():
+    print(5+4)
+    x = 5
+    print( x+5)
+
+
+if __name__ == '__main__':
+    print("hello")
+    main()
